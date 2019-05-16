b29a914e7946cada0cd5b773bd38d8caac248790
CHelfgott <chelfgott@gmail.com>
2019-05-16 11:00:27 -0400

    Update bouncing_ball-pytor.py

--- bouncing_ball-pytor.py
+++ bouncing_ball-pytor.py
@@ -5,26 +5,45 @@ Simple video prediction task -- NN test
 @author: Craig
 """
 
-import cv2, numpy as np, os, random, math, glob
-import sys
+import cv2, numpy as np, os, random, math
+import os.path as osp
 import time
 import datetime
 import skvideo
 import skvideo.io as vidio
 import torch, torch.nn as nn, torch.nn.functional as F, torch.optim as optim
 from torch.autograd import Variable
+import torch.backends.cudnn as cudnn
 import argparse
 
 NUM_DIMS = 3
 
-INPUT_SIZE = 512
-NUM_FRAMES = 1000
+INPUT_SIZE = 128
+NUM_FRAMES = 100
 NUM_EPOCHS = 100
 KERNEL_SIZE = 3
 PADDING = KERNEL_SIZE // 2
 
 def str2bool(v):
     return v.lower() in ("yes", "true", "t", "1")
+
+
+class AverageMeter(object):
+  """Computes and stores the average and current value"""
+  def __init__(self):
+    self.reset()
+
+  def reset(self):
+    self.val = 0
+    self.avg = 0
+    self.sum = 0
+    self.count = 0
+
+  def update(self, val, n=1):
+    self.val = val
+    self.sum += val * n
+    self.count += n
+    self.avg = self.sum / self.count if self.count != 0 else 0
 
 
 class BouncingBall:
@@ -143,7 +162,7 @@ def drawBall(ball, vSizes, lightDirection, frame):
 
   
 def buildBouncingBallVideo(nBalls, vidSize, nFrames):
-  minBallSize = 50
+  minBallSize = 25
   maxBallSize = int(min(vidSize[0], vidSize[1]) / 3)
   balls = []
   vSizes = [vidSize[0], vidSize[1]]
@@ -192,6 +211,7 @@ class Conv2DRNN(nn.Module):
     self.Gates = nn.Conv2d(in_features + hidden_layers,
                            hidden_layers + out_features,
                            KERNEL_SIZE, padding=PADDING)
+    self.activation = nn.LeakyReLU(0.1)
     
   def step(self, input_, prev_hidden):
     # get batch and spatial sizes
@@ -205,9 +225,8 @@ class Conv2DRNN(nn.Module):
     
     # data size is [batch, channel, height, width]
     stacked_inputs = torch.cat((input_, prev_hidden), 1)
-    gates = self.Gates(stacked_inputs)
-    activation = nn.LeakyReLu(0.1)
-    gates = activation(gates)
+    gates = self.Gates(stacked_inputs)  
+    gates = self.activation(gates)
     
     hidden = gates[:, 0:self.hidl, :,:]
     output_layer = gates[:, self.hidl:, :,:]
@@ -220,8 +239,9 @@ class Conv2DRNN(nn.Module):
     outputs = Variable(torch.zeros(list(inputs.data.size()[:2]) + [self.outf] + 
                                    list(inputs.data.size()[3:])))
     for i in range(steps):
-      input = inputs[:,i,...]
+      input = inputs[:,i,...].squeeze()
       hidden, output = self.step(input, hidden)
+      print('Craig Beta: ' + self.outf)
       outputs[:,i,...] = output.unsqueeze(1)
 
     return hidden, outputs
@@ -235,16 +255,16 @@ class VideoNet(nn.Module):
   def __init__(self, **kwargs):
     super(VideoNet, self).__init__()
     
-    self.rnn_layers = {}
-    self.convT = {}
-    self.conv = {}
+    self.rnn_layers = nn.ModuleDict()
+    self.convT = nn.ModuleDict()
+    self.conv = nn.ModuleDict()
     for i in [3, 6, 12, 24, 48]:
-      self.rnn_layers[i] = Conv2DRNN(i, 3, 2*i)
-      self.convT[i] = nn.ConvTranspose2D(2*i, i, kernel_size=3, stride=2)
-      self.conv[i] = nn.Conv2D(3*i, i, kernel_size=KERNEL_SIZE, padding=PADDING)
+      self.rnn_layers[str(i)] = Conv2DRNN(i, 3, 2*i)
+      self.convT[str(i)] = nn.ConvTranspose2d(2*i, i, kernel_size=3, stride=2)
+      self.conv[str(i)] = nn.Conv2d(3*i, i, kernel_size=KERNEL_SIZE, padding=PADDING)
       
-    self.maxpool = nn.MaxPool2D((2,2))
-    self.dropout = nn.DropOut2D(0.1)
+    self.maxpool = nn.MaxPool2d((2,2))
+    self.dropout = nn.Dropout2d(0.1)
 
   def forward(self, batch_input):
     rnn_outputs = {}
@@ -254,33 +274,35 @@ class VideoNet(nn.Module):
     assert(2 ** math.log(height, 2) == height)    # height is a power of 2
     
     for i in [3, 6, 12, 24]:
-      _, rnn_outputs[i] = self.rnn_layers[i].forward(layer)
+      _, rnn_outputs[i] = self.rnn_layers[str(i)].forward(layer)
 	  # rnn_outputs is [NBatch, NFrames, 2*i, H(layer), W(layer)]
       layer = torch.stack([self.dropout(self.maxpool(x)) for x in torch.unbind(rnn_outputs[i], 0)], 0)
 	  # layer is [NBatch, NFrames, 2*i, H/2, W/2]
     
     for i in [24, 12, 6, 3]:
-      layer = torch.stack([self.dropout(self.convT[i].forward(x)) for x in torch.unbind(layer, 0)], 0)
+      layer = torch.stack([self.dropout(self.convT[str(i)].forward(x)) for x in torch.unbind(layer, 0)], 0)
       # layer is [NBatch, NFrames, i, 2*H(layer), 2*W(layer)]
       layer = torch.cat(rnn_outputs[i], layer, 1)
       # layer is [NBatch, NFrames, 3*i, 2*H(layer), 2*W(layer)]
-      layer = torch.stack([self.conv[i].forward(x) for x in torch.unbind(layer, 0)], 0)
+      layer = torch.stack([self.conv[str(i)].forward(x) 
+                           for x in torch.unbind(layer, 0)], 0)
       # layer is [NBatch, NFrames, i, 2*H(layer), 2*W(layer)]
 	
     if not self.training:
       return layer
     
     loss = nn.MSELoss(reduce=False)
-    return loss(batch_inputs, layer)
+    return loss(batch_input, layer)
 	
   
 def train(epoch, video_size, model, optimizer_model, use_gpu,
           samples_per_epoch = 1000, batch_size=25):
   losses = AverageMeter()
   batch_time = AverageMeter()
-  data_time = AverageMeter()
+  end = time.time()
 
-  n_iters = samples_per_epoch / batch_size
+  print("Craig Alpha: {:d},{:d},{:d}".format(batch_size, NUM_FRAMES, video_size))
+  n_iters = int(samples_per_epoch / batch_size)
   for iter in range(n_iters):
     video_inputs = np.zeros([batch_size, NUM_FRAMES, 3, video_size, video_size])
     for i in range(batch_size):
@@ -288,22 +310,25 @@ def train(epoch, video_size, model, optimizer_model, use_gpu,
       video_input = buildBouncingBallVideo(num_balls, 
                                            [video_size, video_size], 
                                            NUM_FRAMES)
-      video_inputs[i,...] = video_input
+      video_inputs[i,...] = np.squeeze(np.stack(np.split(video_input, 3, axis=3), 1))
       
     inputs = Variable(torch.from_numpy(video_inputs).float())
 
     loss = model(inputs)
+    losses.update(loss, batch_size)
 
     optimizer_model.zero_grad()
     loss.backward()
     optimizer_model.step()
 
-    losses[epoch] += loss.data[0]
+    #losses[epoch] += loss.data[0]
+    batch_time.update(time.time() - end)
+    end = time.time()
 
   if epoch > 0:
     print(epoch, loss.data[0])
 
-  end = time.time()
+
  
   
 def main():
@@ -338,9 +363,9 @@ def main():
   if args.use_cpu: use_gpu = False
 
   if not args.evaluate:
-    sys.stdout = Logger(osp.join(args.save_dir, 'log_train.txt'))
+    print(osp.join(args.save_dir, 'log_train.txt'))
   else:
-    sys.stdout = Logger(osp.join(args.save_dir, 'log_test.txt'))
+    print(osp.join(args.save_dir, 'log_test.txt'))
   print("==========\nArgs:{}\n==========".format(args))
 
   if use_gpu:
@@ -356,7 +381,7 @@ def main():
   
   start_epoch = args.start_epoch
 
-  optimizer_model = torch.optim.Adam(model.parameters(), lr=args.lr)
+  optimizer_model = optim.Adam(model.parameters(), lr=args.lr)
   
   if args.resume:
     print("Loading checkpoint from '{}'".format(args.resume))
@@ -380,7 +405,7 @@ def main():
 
   for epoch in range(start_epoch, args.max_epoch):
     start_train_time = time.time()
-    train(epoch, arg.size, model, optimizer_model, use_gpu)
+    train(epoch, args.size, model, optimizer_model, use_gpu)
     train_time += round(time.time() - start_train_time)
         
     if (epoch+1) % 5 == 0 or (epoch+1) == args.max_epoch:
@@ -395,9 +420,8 @@ def main():
         state_dict = model.module.state_dict()
       else:
         state_dict = model.state_dict()
-      save_checkpoint({ 'state_dict': state_dict, 'rank1': rank1, 'epoch': epoch,
-                      }, is_best, 
-                      osp.join(args.save_dir, 'checkpoint_ep' + str(epoch+1) + '.pth.tar'))
+      torch.save({ 'state_dict': state_dict, 'rank1': rank1, 'epoch': epoch, }, is_best, 
+                 osp.join(args.save_dir, 'checkpoint_ep' + str(epoch+1) + '.pth.tar'))
 
     print("==> Best Rank-1 {:.1%}, achieved at epoch {}".format(best_rank1, best_epoch))
 
