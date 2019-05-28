f15a13151d059f5d2fbf15f16a667440e3ae3cd0
CHelfgott <chelfgott@gmail.com>
2019-05-28 14:03:30 -0400

    Update bouncing_ball-pytor.py

--- bouncing_ball-pytor.py
+++ bouncing_ball-pytor.py
@@ -261,7 +261,6 @@ class Conv2DRNN(nn.Module):
     if not hidden is None:
       hidden.cuda(self.device)
     steps = inputs.data.size()[1]
-    print("Conv2DRNN steps {}, #output filters {}".format(steps, self.outf))
     outputs = []
 
     for i in range(steps):
@@ -310,7 +309,6 @@ class VideoNet(nn.Module):
     for i in [24, 12, 6, 3]:
       layer = torch.stack([self.dropout(self.convT[str(i)].forward(x)) for x in torch.unbind(layer, 0)], 0)
       # layer is [NBatch, NFrames, i, 2*H(layer), 2*W(layer)]
-      print("Stacking at step {}: RNN outputs {}, layer {}".format(i, rnn_outputs[i].data.size(), layer.data.size()))
       layer = torch.cat([rnn_outputs[i], layer], dim=2)
       # layer is [NBatch, NFrames, 3*i, 2*H(layer), 2*W(layer)]
       layer = torch.stack([self.conv[str(i)].forward(x) 
