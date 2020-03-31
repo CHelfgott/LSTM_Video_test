dbfabc89760a452a24ba637010f28f94d8d26c3a
CHelfgott <chelfgott@gmail.com>
2020-03-31 14:23:42 -0400

    Minor fix

--- convrnn.py
+++ convrnn.py
@@ -59,7 +59,7 @@ class Conv2DRNNCell(nn.Module):
       raise NotImplementedError()
     else:
       # Since the init is done in forward. Can send image size here
-      hidden = self.init_hidden(batch_size=b, h, w)
+      hidden = self.init_hidden(b, h, w)
 
     outputs = []
 
