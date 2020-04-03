1d01f8bb3da768c03fd54fda1d24e5983f54cddd
CHelfgott <chelfgott@gmail.com>
2020-04-03 14:28:28 -0400

    Adding a little bit of debug to video evaluation

--- bouncing_ball-pytor.py
+++ bouncing_ball-pytor.py
@@ -209,7 +209,10 @@ def test(model, video_size, save_output=False):
 def evaluate(model, video_file):
   model.eval()
   with open(video_file) as vf:
-    video_inputs = np.expand_dims(np.transpose(vidio.vread(vf),(0,3,1,2)), axis=0)
+    vf_data = vidio.vread(vf)
+    print('Test video size: ')
+    print(vf_data.shape())
+    video_inputs = np.expand_dims(np.transpose(vf_data,(0,3,1,2)), axis=0)
   inputs = Variable(torch.from_numpy(video_inputs).float())
   
   loss, outputs = model(inputs)
